2535d5df94f11df6fdc3cc3fdb774db8656dae5e
mykhailopulov <mihail.pulov.247@gmail.com>
2026-05-15 05:34:27 +0300

    feat: implement Serifa font

--- src/main/java/editor/EmbroideryEditor.java
+++ src/main/java/editor/EmbroideryEditor.java
@@ -2,20 +2,22 @@ package editor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class EmbroideryEditor extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // load FXML
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/main_layout.fxml")));
+        // Load custom font explicitly
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Serifa.ttf"), 14);
 
-        Scene scene = new Scene(root, 1200, 800);
+        // load FXML
+        FXMLLoader fxmlLoader = new FXMLLoader(EmbroideryEditor.class.getResource("/main_layout.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
 
         // load CSS
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
